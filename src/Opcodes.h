68cd0f213cf19570a00b743a23bd55e5b0010102
rsredsq <rsredsq@gmail.com>
2017-03-19 17:07:43 +0300

    Fix

--- src/Opcodes.h
+++ src/Opcodes.h
@@ -14,7 +14,7 @@
 #define END_OPS };
 
 #ifdef CHIP_EMU
-
+#define fn(name) fn_##name
 #else
 #define fn(name) nullptr
 #endif
@@ -24,14 +24,7 @@ BEGIN_OPS \
     OP(0x0|nnn, "SYS", None, "[Deprecated] Jump to a machine code routine at nnn.", fn(nop)), \
     OP(0x00E0, "CLS", None, "Clear the display.", fn(0x00E0)), \
     OP(0x00EE, "RET", None, "Return from a subroutine.", fn(0x00EE)), \
-	OP(0x00B|n, "SCU", N, "Scroll display N lines up.", fn(0x00Bn)), \
-	OP(0x00C|n, "SCD", N, "Scroll display N lines down.", fn(0x00Cn)), \
-	OP(0x00FB, "SCR", None, "Scroll display 4 pixels right.", fn(0x00FB)), \
-	OP(0x00FC, "SCL", None, "Scroll display 4 pixels left.", fn(0x00FC)), \
-	OP(0x00FD, "EXIT", None, "Quit the emulator.", fn(0x00FD)), \
-	OP(0x00FE, "LOW", None, "Enable extended screen mode *CHIP48*.", fn(0x00FE)), \
-	OP(0x00FF, "HIGH", None, "Enable extended screen mode *SUPER CHIP*.", fn(0x00FF)), \
-    OP(0x1|nnn, "JP", Addr, "Jump to location nnn.", fn(0x1nnn)), \
+	OP(0x1|nnn, "JP", Addr, "Jump to location nnn.", fn(0x1nnn)), \
     OP(0x2|nnn, "CALL", Addr, "Call subroutine at nnn.", fn(0x2nnn)), \
     OP(0x3|x|kk, "SE", VxByte, "Skip next instruction if Vx = kk.", fn(0x3xkk)), \
     OP(0x4|x|kk, "SNE", VxByte, "Skip next instruction if Vx != kk.", fn(0x4xkk)), \
@@ -63,10 +56,18 @@ BEGIN_OPS \
     OP(0xF|x|3|3, "LD", BVx, "Store BCD representation of Vx in memory locations I, I+1, and I+2.", fn(0xFx33)), \
     OP(0xF|x|5|5, "LD", IVx, "Store registers V0 through Vx in memory starting at location I.", fn(0xFx55)), \
     OP(0xF|x|6|5, "LD", VxI, "Read registers V0 through Vx from memory starting at location I.", fn(0xFx65)), \
-	OP(0xF|x|3|0, "LD", Vx, "Point I to 10 byte numeric sprite for value in VX.", fn(0xFx30)), \
-	OP(0xF|x|7|5, "LD", Vx, "Save V0...VX (X<8) in the RPL flags.", fn(0xFx75)), \
-	OP(0xF|x|8|5, "LD", Vx, "Load V0...VX (X<8) from the HP48 flags.", fn(0xFx85)), \
-    OP(0xFFFF, "Unknown command", None, "Unknown command", fn(nop)) \
+    OP(0xFFFF, "Unknown command", None, "Unknown command", fn(nop)), \
+    /************ Super CHIP-8 functions ************/ \
+    OP(0x00B|n, "SCU", N, "Scroll display N lines up.", fn(0x00Bn)), \
+    OP(0x00C|n, "SCD", N, "Scroll display N lines down.", fn(0x00Cn)), \
+    OP(0x00FB, "SCR", None, "Scroll display 4 pixels right.", fn(0x00FB)), \
+    OP(0x00FC, "SCL", None, "Scroll display 4 pixels left.", fn(0x00FC)), \
+    OP(0x00FD, "EXIT", None, "Quit the emulator.", fn(0x00FD)), \
+    OP(0x00FE, "LOW", None, "Enable extended screen mode *CHIP48*.", fn(0x00FE)), \
+    OP(0x00FF, "HIGH", None, "Enable extended screen mode *SUPER CHIP*.", fn(0x00FF)), \
+    OP(0xF|x|3|0, "LD", Vx, "Point I to 10 byte numeric sprite for value in VX.", fn(0xFx30)), \
+    OP(0xF|x|7|5, "LD", Vx, "Save V0...VX (X<8) in the RPL flags.", fn(0xFx75)), \
+    OP(0xF|x|8|5, "LD", Vx, "Load V0...VX (X<8) from the HP48 flags.", fn(0xFx85)) \
 END_OPS
 
 #define op(n1, n2, n3, n4) (n1 << 12 | n2 << 8 | n3 << 4 | n4)
